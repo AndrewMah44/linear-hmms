727e4161f9cc6e78a816adffc684cf910cd368dc
AndrewMah44 <andrew.mah44@gmail.com>
2026-09-21 12:13:17 -0400

    input index instead of looping. add slurm script

--- training/train_rnn_random_hmm.py
+++ training/train_rnn_random_hmm.py
@@ -2,6 +2,7 @@ import os
 os.environ["JAX_PLATFORMS"] = "cpu"
 
 import time 
+import argparse
 import datetime
 import pickle as pkl
 import jax.random as jr
@@ -40,56 +41,62 @@ conditions = [(alpha_e, alpha_t, ratio, num_states)
 conditions += [(1, 1, 1, 5)]
 
 # ==== Loop Over Conditions ====
-for (alpha_e, alpha_t, ratio, num_states) in conditions[1:]:
-    # ==== Load data ====
-    paths = load_paths_config()
-    datadir = paths["dataset_dir"] / "RandomHMMs"
-    data_filename = f"RandomHMM_"\
-        + f"{num_states}_{alpha_e:0.2f}_{alpha_t:0.2f}_{ratio:0.1f}.pkl"
-    print(f"Data file: {data_filename}", flush=True)
+parser = argparse.ArgumentParser()
+parser.add_argument("--i", type=int, required=True)
+args = parser.parse_args()
 
-    with open(datadir / data_filename, "rb") as file:
-        (hmm, hmm_params, task_params) = pkl.load(file)
+task_idx = args.i
+(alpha_e, alpha_t, ratio, num_states) = conditions[task_idx]
 
-    training_data, validation_data, test_data = generate_hmm_dataset(
-        hmm, hmm_params, dataset_params, task_params)
+# ==== Load data ====
+paths = load_paths_config()
+datadir = paths["dataset_dir"] / "RandomHMMs"
+data_filename = f"RandomHMM_"\
+    + f"{num_states}_{alpha_e:0.2f}_{alpha_t:0.2f}_{ratio:0.1f}.pkl"
+print(f"Data file: {data_filename}", flush=True)
 
-    test_filename = (datadir / data_filename).with_stem(
-        f"{(datadir / data_filename).stem}_test"
-        )
-    with open(test_filename, "wb") as file:
-        pkl.dump(test_data, file)
+with open(datadir / data_filename, "rb") as file:
+    (hmm, hmm_params, task_params) = pkl.load(file)
 
-    # ==== Training Parameters ====
-    model_specs = {
-            'model_class': 'Linear',
-            'in_size': task_params['num_classes'],
-            'hidden_size': num_states,
-            'out_size': task_params['num_classes'],
-            'mlp_depth': 0,
-            'num_blocks': 1
-    }
+training_data, validation_data, test_data = generate_hmm_dataset(
+    hmm, hmm_params, dataset_params, task_params)
 
-    # ==== Train 5 independent seeds ====
-    for idx in range(5):
-        seed = int(time.time())
-        model_specs['key'] = jr.PRNGKey(seed)
+test_filename = (datadir / data_filename).with_stem(
+    f"{(datadir / data_filename).stem}_test"
+    )
+with open(test_filename, "wb") as file:
+    pkl.dump(test_data, file)
 
-        init_model = StackedLinearRNN(**model_specs)
+# ==== Training Parameters ====
+model_specs = {
+        'model_class': 'Linear',
+        'in_size': task_params['num_classes'],
+        'hidden_size': num_states,
+        'out_size': task_params['num_classes'],
+        'mlp_depth': 0,
+        'num_blocks': 1
+}
 
-        # == Saving parameters ==
-        savedir = paths['rnn_data_dir'] / task_params['task']
-        savedir.mkdir(parents=True, exist_ok=True)
+# ==== Train 5 independent seeds ====
+for idx in range(5):
+    seed = int(time.time())
+    model_specs['key'] = jr.PRNGKey(seed)
 
-        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    init_model = StackedLinearRNN(**model_specs)
 
-        filename = savedir / (f"{model_specs['model_class']}_"\
-            + f"{data_filename}_" \
-            + f"{dataset_params['num_timesteps']}_{idx}")
-        print(f"Model file: {filename}", flush=True)
-        
-        model, training_loss_history, validation_loss_history = \
-                train_prediction_model(
-                init_model, training_data, validation_data, 
-                task_params, opt_params, model_specs, filename)
+    # == Saving parameters ==
+    savedir = paths['rnn_data_dir'] / task_params['task']
+    savedir.mkdir(parents=True, exist_ok=True)
+
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    filename = savedir / (f"{model_specs['model_class']}_"\
+        + f"{data_filename}_" \
+        + f"{dataset_params['num_timesteps']}_{idx}")
+    print(f"Model file: {filename}", flush=True)
+    
+    model, training_loss_history, validation_loss_history = \
+            train_prediction_model(
+            init_model, training_data, validation_data, 
+            task_params, opt_params, model_specs, filename)
 
---
+++ training/train_rnn_random_hmm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=rand_hmm
+#SBATCH --output=logs/rand_hmm_%A_%a.out
+#SBATCH --error=logs/rand_hmm_%A_%a.err
+#SBATCH --array=2
+#SBATCH --cpus-per-task=1         # Adjust CPU count as needed
+#SBATCH --mem=14G                 # Adjust memory as needed
+#SBATCH --time=5:00:00            # Max runtime
+#SBATCH --mail-type=END,FAIL,ARRAY_TASKS
+#SBATCH --mail-user=amah@flatironinstitute.org  # Or your actual email
+#SBATCH --partition=genx
+
+# Load your environment
+module --force purge
+module load python
+source ~/venvs/linear_hmms/bin/activate
+
+# Run the script with the array index
+python3 train_rnn_random_hmm.py --i $SLURM_ARRAY_TASK_ID
